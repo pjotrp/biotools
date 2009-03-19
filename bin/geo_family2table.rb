50d4bb07d4b4ca607aa271cb81576ef8a8fe1952
Pjotr Prins <pjotr.public01@thebird.nl>
2009-03-19 11:28:26 +0100

    Write family matrix table

--- bin/geo_family2table.rb
+++ bin/geo_family2table.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/ruby
 #
 #  This script parses a GEO project MINiML (MIAME Notation in Markup Language)
-#  family file and turns the array data into a single matrix file containing
+#  family file and tucnames the array data into a single matrix file containing
 #  a probe descriptor and the raw probe values, as described on
 #
 #      http://www.ncbi.nlm.nih.gov/projects/geo/info/MINiML.html
@@ -69,6 +69,11 @@ $stderr.print "geo_family2table.rb by Pjotr Prins (c) 2009\n"
 
 options = OpenStruct.new()
 opts = OptionParser.new() { |opts|
+  # List of fields
+  opts.on("--fields x,y", Array, "list of field names (default VALUE)") do |fields|
+    options.fields = fields
+  end
+
   opts.on_tail("-h", "--help", "Print this message") {
     print(opts)
     print <<EXAMPLE
@@ -77,7 +82,7 @@ Examples:
 
     geo_family2table.rb GPL3718_family.xml > GPL3718_family.csv
 
-    geo_family2table.rb GSE10940_family.xml > GSE10940_family.csv
+    geo_family2table.rb --fields=VALUE3,VALUE2 GSE10940_family.xml > GSE10940_family.csv
 
 EXAMPLE
     exit()
@@ -111,31 +116,22 @@ ARGV.each do | fn |
     $stderr.print field_names.join(':'),"\n"
     next if sample.rows == 0
     array = []
-    # Make rowname
-    rn = sample.title.strip
-    if rn and rn != ''
-      rn = rn.gsub(/\t/,' ')
-      rn = rn.gsub(/&/,'&amp;')
-      rn = rn.gsub(/\</,'&lt;')
-      rn = rn.gsub(/\>/,'&gt;')
-      rn = rn.gsub(/\"/,'&quote;')
-      rn = rn.gsub(/\'/,'&apos;')
+    # Make column name
+    cname = sample.title.strip
+    if cname and cname != ''
+      cname = cname.gsub(/\t/,' ')
+      cname = cname.gsub(/&/,'&amp;')
+      cname = cname.gsub(/\</,'&lt;')
+      cname = cname.gsub(/\>/,'&gt;')
+      cname = cname.gsub(/\"/,'&quote;')
+      cname = cname.gsub(/\'/,'&apos;')
     end
+    colnames.push "#{sample.acc}:: #{cname}"
 
     if not isTwoColor
-      if not field_names.include?('ID_REF') and not field_names.include?('VALUE')
-        p field_names
-        raise 'Problem with field names - missing value!'
-      end
-      columns = ['ID_REF']
-      if field_names.include?('RAW_SIGNAL')
-        columns.push 'RAW_SIGNAL'
-      else
-        columns.push 'VALUE'
-      end
-      $stderr.print 'Reading: ',columns.join(':'),"\n"
-      colnames.push "#{sample.acc}:: #{rn}"
-      sample.each_row(:columns => columns) do | data |
+      field_id = sample.field_id
+      field_raw = sample.field_raw(options.fields)
+      sample.each_row(:columns => [field_id,field_raw]) do | data |
         rowname = data[0]
         rownames.add(rowname)
         array[rownames.index(rowname)] = data[1]
