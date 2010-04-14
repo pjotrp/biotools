9a142969ffa0b169bdb7169a98d96cf0b2573410
Pjotr Prins <pjotr.public01@thebird.nl>
2010-04-14 13:30:35 +0200

    Update

--- test/lotte/run.sh
+++ test/lotte/run.sh
@@ -8,5 +8,4 @@ for seq in fasta/IL-15.txt fasta/TGFb1.txt ; do
   done
 done
 
-
 # export x=IL-4.txt_PC.txt_s9999_i1000.fa ; ruby -I ~/izip//git/opensource/bigbio/lib -I ../../../bioruby/lib/ ../../bin/mrna_stats --seq fasta/IL-4.txt $x > $x.csv &
