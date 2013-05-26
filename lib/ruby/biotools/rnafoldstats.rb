72342ca0e32478a7f2cb0e0d48adb5208ecf1d0e
Pjotr Prins <pjotr.public01@thebird.nl>
2013-05-26 10:40:46 +0200

    Interpretation moved slightly

--- lib/ruby/biotools/rnafoldstats.rb
+++ lib/ruby/biotools/rnafoldstats.rb
@@ -4,20 +4,20 @@ module RNAfoldStats
   # Calculated linked nucleotides - referred to by dots. In reality
   # the number of links is half this number
   def linked buf
-    buf.count(".")
+    buf.count("(")
   end
 
   def unlinked buf
-    buf.size - linked(buf)
+    buf.count(".")
   end
 
   # linked stretches between eyes
   def stretches buf
-    buf.split(/\.+/).size
+    buf.split(/\.+/).size/2
   end
 
   def avg_stretch_size buf
-    unlinked(buf).to_f/stretches(buf)
+    linked(buf).to_f/stretches(buf)
   end
 
 end
@@ -30,23 +30,23 @@ if $UNITTEST
     FOLD = '...((((((((((((.(((..((((((........(((((.(......).........(((((((.((...(((((((..(((((...(((((.....)))))..).)))).))))))).)).)))))))..(((.....))).))))).........)))))).)))))))))))))))...'
 
     def test_linked
-      assert_equal(2,unlinked("()"))
-      assert_equal(2,unlinked("(.)"))
-      assert_equal(1,linked("."))
-      assert_equal(112,unlinked(FOLD))
-      assert_equal(71,linked(FOLD))
+      assert_equal(0,unlinked("()"))
+      assert_equal(1,unlinked("(.)"))
+      assert_equal(0,linked("."))
+      assert_equal(71,unlinked(FOLD))
+      assert_equal(56,linked(FOLD))
     end
    
     def test_stretches
-      assert_equal(2,stretches("(.)"))
-      assert_equal(3,stretches(".(.)"))
-      assert_equal(3,stretches(".(.)."))
-      assert_equal(23,stretches(FOLD))
+      assert_equal(1,stretches("(.)"))
+      assert_equal(1,stretches(".(.)"))
+      assert_equal(1,stretches(".(.)."))
+      assert_equal(11,stretches(FOLD))
     end
 
     def test_stretchesize
       assert_equal(1,avg_stretch_size("(.)"))
-      assert_equal("4.8695652173913",avg_stretch_size(FOLD).to_s)
+      assert_equal("5.09090909090909",avg_stretch_size(FOLD).to_s)
     end
   end
 
