2244f49d7021369a8fb1f2c32d79f0eaa7f62c0b
Pjotr Prins <pjotr.public01@thebird.nl>
2013-05-27 21:18:14 +0200

    Stats

--- lib/ruby/biotools/rnafoldstats.rb
+++ lib/ruby/biotools/rnafoldstats.rb
@@ -34,26 +34,26 @@ if $UNITTEST
       assert_equal(1,unlinked("(.)"))
       assert_equal(0,linked("."))
       assert_equal(71,unlinked(FOLD))
-      assert_equal(56,linked(FOLD))
+      assert_equal(112,linked(FOLD))
       assert_equal(372,unlinked(FOLD2))
-      assert_equal(246,linked(FOLD2))
+      assert_equal(492,linked(FOLD2))
       assert_equal(246,FOLD2.count(')'))
       assert_equal(246,FOLD2.count('('))
       assert_equal(372,FOLD2.count('.'))
     end
    
     def test_stretches
-      assert_equal(1,stretches("(.)"))
-      assert_equal(1,stretches(".(.)"))
-      assert_equal(1,stretches(".(.)."))
-      assert_equal(11,stretches(FOLD))
-      assert_equal(47,stretches(FOLD2))
+      assert_equal(2,stretches("(.)"))
+      assert_equal(3,stretches(".(.)"))
+      assert_equal(3,stretches(".(.)."))
+      assert_equal(23,stretches(FOLD))
+      assert_equal(95,stretches(FOLD2))
     end
 
     def test_stretchesize
       assert_equal(1,avg_stretch_size("(.)"))
-      assert_equal("5.09090909090909",avg_stretch_size(FOLD).to_s)
-      assert_equal("5.23404255319149",avg_stretch_size(FOLD2).to_s)
+      assert_equal("4.8695652173913",avg_stretch_size(FOLD).to_s)
+      assert_equal("5.17894736842105",avg_stretch_size(FOLD2).to_s)
     end
   end
 
--- lib/ruby/biotools/rnastats.rb
+++ lib/ruby/biotools/rnastats.rb
@@ -23,12 +23,12 @@ class RNAfolds < Array
   # Run rnafold to fetch energy and fold sequence
   def initialize seq, templist
     templist.each do | t |
-      result = fold_info(seq,t)
-      e1 = fold_energy(result)
+      fold_result = fold_info(seq,t)
+      e1 = fold_energy(fold_result)
       # p result
       # short = result.gsub(/\(\.+\)/,'').gsub(/[\(\)]/,'')
       # p [short,short.size]
-      rec = { :temp => t, :energy => e1, :fold => fold_seq(result), :size => result.size, :linked => linked(result), :stretches => stretches(result), :avg_stretch_size => avg_stretch_size(result) }
+      rec = { :temp => t, :energy => e1, :fold => fold_seq(fold_result), :size => fold_result.size, :linked => linked(fold_result), :stretches => stretches(fold_result), :avg_stretch_size => avg_stretch_size(fold_result) }
       push rec
       # @maxtemp = t if @maxtemp==nil or @maxtemp<t
     end
@@ -109,7 +109,7 @@ class RNAStats
         print "\tstretches@",t,"C"
       end
       @templist.each do | t |
-        print "\tisl_size@",t,"C"
+        print "\tstretch_size@",t,"C"
       end
       @templist.each do | t |
         print "\tE@",t,"C"
